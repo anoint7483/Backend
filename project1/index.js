7bdb1d4773ed0cbe319ce8fc940871d8d5c0f7ab
anoint7483 <yabhishek8774@gmail.com>
2025-03-03 22:41:07 +0530

    new project created

--- project1/index.js
+++ project1/index.js
@@ -1,15 +1,21 @@
+
 const express = require('express');
 const app = express();
+path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.get('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-    res.send("chal raha hai");
+    res.render("index");
+});
 
+app.get("/profile/abhi", function(req, res){
+    res.render("chal raha hai");
 })
 
 app.listen(3000, function(){
     console.log("it is running on port 3000");
-})
+}) 
